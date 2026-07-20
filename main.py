637c3ed97acf2a3518a166e1efeb3216851f9476
Abdullah1dev <242677@students.au.edu.pk>
2026-07-20 13:03:51 +0500

    Done Explloratory Data Analysis to analyze all the data

--- main.py
+++ main.py
@@ -11,3 +11,43 @@ print(iris.feature_names)
 
 print("Target Names")
 print(iris.target_names)
+
+
+df = pd.DataFrame(
+    data = iris.data,
+    columns = iris.feature_names
+)
+df["species"] = iris.target
+
+
+
+
+df["species"] = df["species"].replace({
+    0 : "setosa",
+    1 : "versicolor",
+    2 : "virgincia"
+}
+    
+)
+
+print(df.head())
+
+#EDA 
+
+print("DF Shape \n")
+print(df.shape)
+
+print("Dataset Information \n")
+print(df.info())
+
+print("Statistical Summary \n")
+print(df.describe())
+
+print("Missing Values")
+print(df.isnull().sum())
+
+print("Specie Distribution \n")
+print(df["species"].value_counts())
+
+
+
